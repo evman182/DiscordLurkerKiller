f16b716d9290d22fbe4ffd6f8f861e3f7a524c41
Evan Raffel <evman182@gmail.com>
2019-08-29 10:12:37 -0400

    cleanup and make actually kick

--- src/DiscordLurkerKiller/Program.cs
+++ src/DiscordLurkerKiller/Program.cs
@@ -11,30 +11,28 @@ namespace DiscordLurkerKiller
 {
     class Program
     {
-        private static readonly HttpClient HttpClient = new HttpClient();
         private static readonly ulong GuildId = ulong.Parse(ConfigurationManager.AppSettings["DiscordGuildId"]);
         private static readonly string DiscordBotToken = ConfigurationManager.AppSettings["DiscordBotToken"];
-        private static readonly ulong PurgeLogChannelId = ulong.Parse(ConfigurationManager.AppSettings["PurgeChannelId"]);
         private static readonly List<ulong> SafeRoleIds = GetSafeRoleIds();
         private static readonly DiscordRestClient DiscordClient = new DiscordRestClient();
         private static readonly DateTime CurrentTime = DateTime.UtcNow;
-        private static readonly List<ulong> SafeUserIds = GetSafeUserIds();
-        private const int MinimumAccountAge = 60;
+        private const int MinimumAccountAge = 32;
         private static readonly ulong MemberRoleId = ulong.Parse(ConfigurationManager.AppSettings["MemberRoleId"]);
-
-
+        
         static async Task Main(string[] args)
         {
             await DiscordClient.LoginAsync(TokenType.Bot, DiscordBotToken);
             var guild = await DiscordClient.GetGuildAsync(GuildId);
-            var users = await guild.GetUsersAsync().FlattenAsync();
-            var usersWithoutMemberRole = users.Where(u => !u.IsBot && !u.RoleIds.Contains(MemberRoleId)).ToList();
-            
-            var joinDateRetriever = new JoinDateRetriever(HttpClient, GuildId, DiscordBotToken);
-            var joinDates = await joinDateRetriever.GetJoinDatesAsync();
+            var users = (await guild.GetUsersAsync().FlattenAsync()).ToList();
 
+            var usersWithoutMemberRole = users.Where(u => u.Id != guild.OwnerId && !u.IsBot && !u.RoleIds.Contains(MemberRoleId)).ToList();
+            
             var accountAgeBoundary = CurrentTime.AddDays(-1 * MinimumAccountAge);
-            var usersToKick = usersWithoutMemberRole.Where(u => joinDates[u.Id] <= accountAgeBoundary).ToList();
+            var usersToKick = usersWithoutMemberRole
+                .Where(u => u.JoinedAt.Value <= accountAgeBoundary
+                            && !u.RoleIds.Any(r => SafeRoleIds.Contains(r))
+                            )
+                .ToList();
 
             var userStrings = usersToKick.Select(r =>
                 r.Username + "#" + r.Discriminator +
@@ -42,21 +40,17 @@ namespace DiscordLurkerKiller
                     ? string.Empty
                     : $" ({r.Nickname})"));
             var usersToKickString = string.Join(Environment.NewLine, userStrings.OrderBy(u => u));
+            
+            foreach (var user in usersToKick)
+            {
+                await user.KickAsync();
+            }
         }
 
         private static List<ulong> GetSafeRoleIds()
         {
             var safeRoleIdsString = ConfigurationManager.AppSettings["SafeRoleIds"];
             if(string.IsNullOrWhiteSpace(safeRoleIdsString))
-                return new List<ulong>();
-
-            return safeRoleIdsString.Split(',').Select(ulong.Parse).ToList();
-        }
-
-        private static List<ulong> GetSafeUserIds()
-        {
-            var safeRoleIdsString = ConfigurationManager.AppSettings["SafeUserIds"];
-            if (string.IsNullOrWhiteSpace(safeRoleIdsString))
                 return new List<ulong>();
 
             return safeRoleIdsString.Split(',').Select(ulong.Parse).ToList();
